996ef8f6efa59dacd1a083516daf1a99bf58cde3
kinn <ksy535760@gmail.com>
2024-03-13 12:27:01 +0900

    Add files via upload

--- src/app/handleText.js
+++ src/app/handleText.js
@@ -1,16 +1,64 @@
-function handleRoadMap() {
-    const titleElement = document.querySelector(".title");
-    const subTitleElement = document.querySelector(".sub-title");
-    const aboutMeElement = document.querySelector(".intro-text");
+function handleAboutMe() {
+    const mottoElement = document.querySelector(".motto-text");
+    const usaElement = document.querySelector(".usa-element");
 
     if (window.innerWidth < 850) {
-        titleElement.innerHTML =
-            "당신도 무언가를<br />상상해본 적이<br />있나요?";
-        subTitleElement.innerHTML =
-            "우리는 상상을<br />현실로 구현합니다.<br /><strong>RoadMap</strong>은 교내 유일<br />웹 개발 동아리입니다.<br /><strong>RoadMap</strong>과 함께라면<br />당신의 상상은 현실이 됩니다.";
-        aboutMeElement.innerHTML =
-            "<strong>RoadMap</strong>은 교내 유일 웹 개발 동아리이며, <br /> 웹 개발을 학습하고 함께 실습하며 <br />웹 개발자로서의 역량을 쌓아나가는 동아리입니다. <br /> <br /> 여러분의 웹 사이트를 만들고 동아리 자체 서버와 <br /> 도메인으로 웹을 호스팅하여 전 세계에서<br />여러분의 웹 페이지에 접속해 마법 같은<br />경험을 할 수 있도록 만듭니다. <br /> <br /> 또한 동아리 내부에서 개인·팀 프로젝트를 통해 <br /> 리더십 및 협업 능력을 향상시키고, <br /> 해커톤과 같은 대회에 참가하여 부원들의 <br /> 실력과 <strong>RoadMap</strong>의 명성을 높입니다.";
+        mottoElement.innerHTML =
+            '<strong>"The best way to<br />predict the future is to create it."</strong>';
+        usaElement.innerHTML =
+            "<strong>the United States<br />of America.</strong>";
     }
 }
 
-export { handleRoadMap };
+function handlePortfolio() {
+    const ileonangElement = document.querySelector(".ileonang-element");
+    const qrElement = document.querySelector(".qr-element");
+    const converterElement = document.querySelector(".converter-element");
+    const RoadMapElement = document.querySelector(".roadmap-element");
+
+    if (window.innerWidth < 850) {
+        ileonangElement.innerHTML =
+            "Made an <strong>Alarm App</strong>, which<br />wakes up user by sending<br />risky message to someone.";
+        qrElement.innerHTML =
+            "Making a <strong>QR Code Generate<br />Service</strong>, which generates<br />& downloads QR Code.";
+        converterElement.innerHTML =
+            "Making an <strong>Exchange Rate Conversion Service</strong>, which<br />calculates by actual exchange rate.";
+        RoadMapElement.innerHTML =
+            "Operating <strong>RoadMap</strong>, which is<br />Web Club of my high school.";
+    }
+}
+
+function handleExperience() {
+    const highthonElement = document.querySelector(".highthon-element");
+
+    if (window.innerWidth < 850) {
+        highthonElement.innerHTML =
+            "Got a <strong>Grand Prize</strong> at <strong>Highthon</strong>, which is hackathon<br />of high school student.";
+    }
+}
+
+function handleAboutMachim() {
+    const aboutMachimElement = document.querySelector(".about-machim");
+
+    if (window.innerWidth < 850) {
+        aboutMachimElement.innerHTML =
+            "<strong>.마침이</strong>는 <strong>Kinn</strong>이 개발한<br />Discord Bot 입니다. <br /> <strong>.마침이</strong>는 당신의 Discord<br />Server를 <strong>관리</strong>할 수 있으며, <br /> <strong>유틸리티</strong> 기능 또한 있어 최고의<br />Discord Bot 사용 경험을 제공합니다. <br /> 다재다능한 Discord Bot, <strong>.마침이</strong>를<br />지금 당신의 서버에서 사용해보세요!";
+    }
+}
+
+function handleKjs() {
+    const aboutMeElement = document.querySelector(".about-me");
+
+    if (window.innerWidth < 850) {
+        aboutMeElement.innerHTML =
+            "저는 <strong>김진성</strong>이고, 학생 <strong>Prouct<br />Manager</strong>입니다. <br /> 저는 미래 세상의 패러다임에<br />혁신을 주는 프로덕트를 만들 것이고, <br />제 프로덕트를 발전시키기 위해<br />많은 노력을 기울이고 있습니다.";
+    }
+}
+
+export {
+    handleAboutMe,
+    handlePortfolio,
+    handleExperience,
+    handleAboutMachim,
+    handleKjs,
+};
