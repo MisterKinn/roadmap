1fbbd0587e776737edad8b0f7aceebc48c85dc55
kinn <ksy535760@gmail.com>
2024-11-18 15:42:39 +0900

    Highlight About Text

--- src/components/home/mobile/About.tsx
+++ src/components/home/mobile/About.tsx
@@ -31,11 +31,11 @@ function About() {
                             </span>
                         </div>
                         <div className="mobile-card-text">
-                            부원들끼리 자유롭게 아이디어를 공유합니다.
+                        부원들끼리 <span className="white">자유롭게 아이디어를 공유</span>합니다.
                             <br />
-                            서로 아이디어를 교환하는 과정에서
+                            서로 아이디어를 <span className="white">교환</span>하는 과정에서
                             <br />
-                            창의적 사고가 확장됩니다.
+                            <span className="white">창의적인 사고</span>가 <span className="white">확장</span>됩니다.
                         </div>
                     </div>
                 </div>
@@ -54,10 +54,11 @@ function About() {
                             </span>
                         </div>
                         <div className="mobile-card-text">
-                            온라인 기반 학습 시스템을 통해
+                        <span className="white">온라인 기반 학습 시스템</span>을 통해
                             <br />
-                            때와 장소에 상관없이 언제든지
-                            <br />웹 개발 지식을 학습할 수 있습니다.
+                            <span className="white">때</span>와 <span className="white">장소</span>에 상관없이 언제든지
+                            <br />
+                            <span className="white">웹 개발 지식</span>을 <span className="white">학습</span>할 수 있습니다.
                         </div>
                     </div>
                 </div>
@@ -72,15 +73,15 @@ function About() {
                                 className="mobile-card-img"
                             />
                             <span className="mobile-card-title">
-                                필요한 정보를 제공합니다
+                            다양한 지원을 제공합니다
                             </span>
                         </div>
                         <div className="mobile-card-text">
-                            개발에 필요한 정보를 항시 제공하여
+                            <span className="white">동아리 예산</span>으로 도서, 강의 등 <span className="white">원하는 것</span>을
                             <br />
-                            정보의 부족으로 인한 막막함을 해소하여
+                            구매하고 <span className="white">동아리 도메인</span>과 <span className="white">서버</span>를 <span className="white">평생 무료</span>로
                             <br />
-                            개발에만 집중할 수 있도록 돕습니다.
+                            사용하는 등 <span className="white">다양한 복지</span>를 제공합니다.
                         </div>
                     </div>
                 </div>
@@ -99,11 +100,11 @@ function About() {
                             </span>
                         </div>
                         <div className="mobile-card-text">
-                            필수 동아리 시간 외 참여를 없앰으로서
+                        <span className="white">필수 동아리 시간 외 참여</span>를 <span className="white">강제하지 않음</span>으로써
                             <br />
-                            누구나 편안한 때와 장소에서 개발하며
+                            누구나 <span className="white">편안한 때</span>와 <span className="white">장소</span>에서 개발하며
                             <br />
-                            개발의 효율성을 극대화하도록 돕습니다.
+                            <span className="white">개발의 효율성</span>을 <span className="white">극대화</span>하도록 돕습니다.
                         </div>
                     </div>
                 </div>
@@ -134,11 +135,11 @@ function About() {
                             </span>
                         </div>
                         <div className="mobile-card-text">
-                            최고의 동료들과 함께 프로젝트를 진행하며
+                        <span className="white">최고의 동료</span>들과 함께 <span className="white">프로젝트</span>를 진행하며
                             <br />
-                            개발 실력을 더욱 발전시키고
+                            <span className="white">개발 실력</span>을 더욱 <span className="white">발전</span>시키고
                             <br />
-                            소통 역량 또한 향상시킵니다.
+                            <span className="white">소통 역량</span> 또한 <span className="white">향상</span>시킵니다.
                         </div>
                     </div>
                 </div>
@@ -157,11 +158,11 @@ function About() {
                             </span>
                         </div>
                         <div className="mobile-card-text">
-                            개발 중 막히는 부분에 봉착했을 때
+                            개발 중 <span className="white">막히는 부분</span>에 봉착했을 때
                             <br />
-                            훌륭한 실력을 가진 동료들이 함께 고민하고
+                            <span className="white">훌륭한 실력</span>을 가진 <span className="white">동료</span>들이 함께 <span className="white">고민</span>하고
                             <br />
-                            양질의 해결책을 제공해줍니다.
+                            <span className="white">양질의 해결책</span>을 제공해줍니다.
                         </div>
                     </div>
                 </div>
@@ -180,11 +181,11 @@ function About() {
                             </span>
                         </div>
                         <div className="mobile-card-text">
-                            부원 간 코드 리뷰 세션을 진행하여
+                            부원 간 <span className="white">코드 리뷰 세션</span>을 진행하여
                             <br />
-                            코드의 가독성과 효율성을 개선하고
+                            코드의 <span className="white">가독성</span>과 <span className="white">효율성</span>을 개선하고
                             <br />
-                            양질의 코드를 작성하도록 돕습니다.
+                            <span className="white">양질의 코드</span>를 작성하도록 돕습니다.
                         </div>
                     </div>
                 </div>
@@ -203,10 +204,11 @@ function About() {
                             </span>
                         </div>
                         <div className="mobile-card-text">
-                            열심히 활동하는 동료와 함께하면서
+                            <span className="white">열심히 활동하는 동료</span>와 함께하면서
                             <br />
-                            끊임없이 자극을 받으며 본인의 역량을 향상시키고,
-                            <br />그 결실을 부원들과 나누기 위해 노력합니다.
+                            끊임없이 <span className="white">자극</span>을 받으며 본인의 <span className="white">역량</span>을 <span className="white">향상</span>시키고,
+                            <br />
+                            그 <span className="white">결실</span>을 부원들과 나누기 위해 노력합니다.
                         </div>
                     </div>
                 </div>
